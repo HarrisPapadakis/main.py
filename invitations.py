c55721e8fe90b38778726a938ae517535a37bf3a
Harris_Papadakis <162270644+HarrisPapadakis@users.noreply.github.com>
2024-11-27 13:46:19 +0200

    Add files via upload

---
+++ invitations.py
@@ -0,0 +1,10 @@
+Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================ RESTART: C:/Users/harri/Desktop/invitations.py ================
+Michael Jordan, I'd like to invite you to my private party!
+Lebron James, I'd like to invite you to my private party!
+Scottie Pipen, I'd like to invite you to my private party!
+Alen Iverson, I'd like to invite you to my private party!
+Steph Curry, I'd like to invite you to my private party!
+Dennis Rodman, I'd like to invite you to my private party!
---
+++ new_invitations_with_available_guests.py
@@ -0,0 +1,16 @@
+Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=== RESTART: C:\Users\harri\Desktop\new_invitations_with_available_guests.py ===
+
+Updated invitations:
+Michael Jordan, I'd like to invite you to my private party!
+Lebron James, I'd like to invite you to my private party!
+Alen Iverson, I'd like to invite you to my private party!
+Steph Curry, I'd like to invite you to my private party!
+Dennis Rodman, I'd like to invite you to my private party!
+Kobe Bryant, I'd like to invite you to my private party!
+Unfortunately, Scottie Pipen can't make it to the dinner.
+
+
+
